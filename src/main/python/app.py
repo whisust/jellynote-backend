85e78db728fad095a1e1b64ab6a1430d1167fbe5
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 00:07:58 +0200

    Update main class with routes and models changes

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -3,16 +3,24 @@ import argparse
 from flask import Flask
 
 import config
-import users
+import routes
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--conf", help="path to conf file")
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    args = parser.parse_args()
 
+def run():
+    args = parser.parse_args()
     conf = config.load(args.conf)
-    app.register_blueprint(users.users_bp, url_prefix='/users')
+
+    app.register_blueprint(routes.users, url_prefix='/users')
+
     app.run(port=conf["server"]["port"], debug=True)
+
+    #todo configure for prod run and not only debug
+
+
+if __name__ == '__main__':
+    run()
