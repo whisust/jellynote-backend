664242e317507e881447725d07c5829cf2c4cc82
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 18:38:06 +0200

    Add update method in DB

--- api/persist/users.py
+++ api/persist/users.py
@@ -21,6 +21,12 @@ _insert_query = "INSERT INTO " + Users.name + " (name, email, instruments) VALUE
 _find_one_query = "SELECT " + Users.all_fields() + " FROM " + Users.name + " WHERE id=%s"
 
 
+def _update_query(req: UserUpdateRequest):
+    fields = [f + ' = %s' for f in req.__dataclass_fields__ if req.__getattribute__(f) is not None]
+
+    return "UPDATE " + Users.name + " SET " + ', '.join(fields) + ", updated_at = now() WHERE id = %s" + _returning_clause
+
+
 def list_all(limit: int) -> List[User]:
     with new_transaction() as cur:
         cur.execute(_list_query(limit))
@@ -47,3 +53,18 @@ def find(user_id: UserId) -> Optional[User]:
             return User.from_row(row)
         else:
             return None
+
+
+def update(user_id: UserId, req: UserUpdateRequest) -> User:
+    with new_transaction() as cur:
+        try:
+            update_params = [i for i in
+                         [req.name, req.email, encode_enum_iterable(req.instruments)] if i is not None
+                         ] + [user_id]
+            cur.execute(_update_query(req), tuple(update_params))
+            return User.from_row(cur.fetchone())
+        except psycopg2.Error as e:
+            if is_unique_constraint_violation(e):
+                raise UpdateError("A user with email=" + req.email + " already exists")
+            else:
+                raise
