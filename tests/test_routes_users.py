9322fd83fb3a488d0d7f711b84e9c926fe54c47c
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 19:40:20 +0200

    Add tests on user update

--- tests/test_routes_users.py
+++ tests/test_routes_users.py
@@ -4,6 +4,12 @@ from random_utils import *
 from fixtures import client
 
 
+def _or(a, b):
+    return b if a is None else a
+
+
+# POST /users
+
 def test_user_creation(client):
     req = random_user_creation_request()
     instruments_as_str = [i.value for i in req.instruments]
@@ -70,3 +76,123 @@ def test_existing_user_creation_returns_conflict(client):
     })
 
     assert result.status_code == 409
+
+
+# PUT /users/:id
+
+def test_update_user_name(client):
+    # should be in another fixture but flemme
+    creation = random_user_creation_request()
+    instruments_as_str = [i.value for i in creation.instruments]
+    creation_result = client.post('/users', json={
+        "name": creation.name,
+        "email": creation.email,
+        "instruments": instruments_as_str
+    })
+
+    user_id = creation_result.json["id"]
+    name = "newname"
+    update_result = client.put('/users/' + str(user_id), json={
+        "name": name
+    })
+
+    assert update_result.status_code == 200
+    assert update_result.json['name'] == name
+    assert update_result.json['email'] == creation.email
+    assert update_result.json['instruments'] == instruments_as_str
+
+
+def test_update_user_email(client):
+    # should be in another fixture but flemme
+    creation = random_user_creation_request()
+    instruments_as_str = [i.value for i in creation.instruments]
+    creation_result = client.post('/users', json={
+        "name": creation.name,
+        "email": creation.email,
+        "instruments": instruments_as_str
+    })
+
+    user_id = creation_result.json["id"]
+    email = random_mail()
+    update_result = client.put('/users/' + str(user_id), json={
+        "email": email
+    })
+
+    assert update_result.status_code == 200
+    assert update_result.json['name'] == creation.name
+    assert update_result.json['email'] == email
+    assert update_result.json['instruments'] == instruments_as_str
+
+
+def test_update_user_instruments(client):
+    # should be in another fixture but flemme
+    creation = random_user_creation_request()
+    creation_result = client.post('/users', json={
+        "name": creation.name,
+        "email": creation.email,
+        "instruments": [Instrument.Guitar.value]
+    })
+
+    user_id = creation_result.json["id"]
+    update_result = client.put('/users/' + str(user_id), json={
+        "instruments": [Instrument.Violin.value]
+    })
+
+    assert update_result.status_code == 200
+    assert update_result.json['name'] == creation.name
+    assert update_result.json['email'] == creation.email
+    assert update_result.json['instruments'] == [Instrument.Violin.value]
+
+
+def test_update_user(client):
+    # should be in another fixture but flemme
+    creation = random_user_creation_request()
+    instruments_as_str = [i.value for i in creation.instruments]
+    creation_result = client.post('/users', json={
+        "name": creation.name,
+        "email": creation.email,
+        "instruments": instruments_as_str
+    })
+
+    user_id = creation_result.json["id"]
+    req = random_user_update_request()
+    name = random_string(5) # we force the name to ensure no 3 emtpy params
+    updated_instruments_as_str = [i.value for i in req.instruments] if req.instruments is not None else None
+    update_result = client.put('/users/' + str(user_id), json={
+        "name": name,
+        "email": req.email,
+        "instruments": updated_instruments_as_str
+    })
+
+    assert update_result.status_code == 200
+    assert update_result.json['name'] == name
+    assert update_result.json['email'] == _or(req.email, creation.email)
+    assert update_result.json['instruments'] == _or(updated_instruments_as_str, instruments_as_str)
+
+
+def test_update_user_conflict(client):
+    """Create two users, try to update email of first one with the second user's email -> conflict"""
+    # should be in another fixture but flemme
+    creation = random_user_creation_request()
+    instruments_as_str = [i.value for i in creation.instruments]
+    creation_result = client.post('/users', json={
+        "name": creation.name,
+        "email": creation.email,
+        "instruments": instruments_as_str
+    })
+
+    creation2 = random_user_creation_request()
+    instruments_as_str = [i.value for i in creation2.instruments]
+    creation_result2 = client.post('/users', json={
+        "name": creation2.name,
+        "email": creation2.email,
+        "instruments": instruments_as_str
+    })
+
+    user_id = creation_result.json["id"]
+    req = UserUpdateRequest(name=None, email=creation2.email, instruments=None)
+    update_result = client.put('/users/' + str(user_id), json={
+        "email": req.email,
+    })
+
+    assert update_result.status_code == 409
