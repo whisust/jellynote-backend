86c9c3c48b52607744539ad7734ac17366f33582
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 23:26:37 +0200

    Cleanup main script for production run

--- api/app.py
+++ api/app.py
@@ -1,36 +1,20 @@
-import argparse
-
 from flask import Flask
 
-import config
+from config import conf
 import routes
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--conf", help="path to conf file", default=None)
-
-
-def parse_conf():
-    global conf
-    args = parser.parse_args()
-    conf = config.load(args.conf)
 
 
 def create_app():
     app = Flask(__name__)
-    # TODO
-    # app.config.from_pyfile(config_filename)
+    print("Starting app with conf")
+    print(app.config)
+    print(conf)
     app.register_blueprint(routes.users, url_prefix='/users')
     app.register_blueprint(routes.songs, url_prefix='/songs')
     return app
 
 
-def run():
-    parse_conf()
-    app = create_app()
-    app.run(port=conf["server"]["port"], debug=True)
-
-    # todo configure for prod run and not only debug
-
+app = create_app()
 
 if __name__ == '__main__':
-    run()
+    app.run(port=conf.server.port, host=conf.server.host, debug=conf.env.lower() == 'dev')
