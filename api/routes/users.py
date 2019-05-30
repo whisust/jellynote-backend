3e602144cb6a72b3aeb511b50f11d450d10d7984
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 19:40:07 +0200

    Add user update route

--- api/routes/users.py
+++ api/routes/users.py
@@ -3,7 +3,7 @@ from flask import request, make_response, Blueprint
 from models.errors import map_error, NotFoundError
 from models.jellynote import UserId
 from models.requests import UserCreationRequestSchema, UserUpdateRequestSchema
-from persist import users, InsertionError
+from persist import users, InsertionError, UpdateError
 from .utils import json_response
 
 users_bp = Blueprint('users', __name__)
@@ -19,9 +19,26 @@ def index():
 @users_bp.route('/<int:user_id>', methods=['PUT', 'DELETE'])
 def user(user_id):
     if request.method == 'PUT':
-        return "Not implemented " + str(user_id), 500
-    else:
+        return update_user(UserId(user_id), request.json)
+    else: # DELETE
         return "Still not implemented " + str(user_id), 500
+
+
+def update_user(user_id: UserId, data):
+    try:
+        user_req = UserUpdateRequestSchema.load(data)
+        user_req.validate()
+        user = users.find(user_id)
+        if user is None:
+            response, code = (NotFoundError("user_id " + str(user_id) + " not found"), 404)
+        else:
+            updated_user = users.update(user_id, user_req)
+            response, code = (updated_user, 200)
+    except UpdateError as e:
+        response, code = (e, 409)
+    except Exception as e:
+        response, code = map_error(e)
+    return json_response(response, code)
 
 
 def create_user(data):
