1eace3344b40195432ab9db0eb0570b47b2e7ca9
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 20:57:39 +0200

    Replace song.name by title

--- resources/migrations/V4__create_songs_table.py
+++ resources/migrations/V4__create_songs_table.py
@@ -5,7 +5,7 @@ step(
     CREATE TABLE IF NOT EXISTS songs
 (
     id          SERIAL    NOT NULL PRIMARY KEY,
-    name        VARCHAR   NOT NULL,
+    title       VARCHAR   NOT NULL,
     instruments TEXT[]    NOT NULL,
     created_at  TIMESTAMP NOT NULL DEFAULT now(),
     updated_at  TIMESTAMP NOT NULL DEFAULT now()
