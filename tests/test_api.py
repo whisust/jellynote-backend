53e48d4c17699f7e7a2e7d3680671950841124fc
Antoine Latrille <antoine.latrille@gmail.com>
2019-05-30 06:38:05 +0200

    Add first tests for users endpoint in api

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,18 @@
 import pytest
-from api.app import flask_app
+from api.app import create_app
 
 
 @pytest.fixture
 def client():
-    flask_app.config['TESTING'] = True
-    client = flask_app.test_client()
+    app = create_app()
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    ctx = app.app_context()
+    ctx.push()
     yield client
+
+    ctx.pop()
 
 
 def test_user_creation(client):
@@ -16,5 +22,40 @@ def test_user_creation(client):
         "instruments": ["guitar", "piano"]
     })
 
-    result
-    assert False
+    assert result.status_code == 200
+    assert result.json["name"] == "testestes"
+    assert result.json["email"] == "email@me.com"
+    assert result.json["instruments"] == ['Guitar', 'Piano']
+    assert result.json["created_at"] is not None
+    assert result.json["updated_at"] is not None
+
+def test_user_creation_bad_email(client):
+    result = client.post('/users', json={
+        "name": "testestes",
+        "email": "aintamail",
+        "instruments": ["guitar", "piano"]
+    })
+
+    assert result.status_code == 400
+    assert result.json["message"] == "Invalid field : email is incorrect"
+
+
+def test_user_creation_empty_name(client):
+    result = client.post('/users', json={
+        "name": "",
+        "email": "email@me.com",
+        "instruments": ["guitar", "piano"]
+    })
+
+    assert result.status_code == 400
+    assert result.json["message"] == "Invalid field : name should not be empty"
+
+def test_user_creation_empty_instruments(client):
+    result = client.post('/users', json={
+        "name": "john",
+        "email": "email@me.com",
+        "instruments": []
+    })
+
+    assert result.status_code == 400
+    assert result.json["message"] == "Invalid field : instruments should not be empty"
